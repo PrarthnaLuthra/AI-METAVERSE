5bbf90dd08cfcc9c753dda20687935cef0d992ad
Prarthna <prarthnaluthra@gmail.com>
2023-04-02 15:20:05 +0530

    CUSTOMIZER

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { useSnapshot } from "valtio";
+import config from "../config/config";
+import state from "../store";
+import { download } from "../assets";
+import { downloadCanvasToImage, reader } from "../config/helpers";
+import { EditorTabs, FilterTabs, DecalTypes } from "../config/constants";
+import { fadeAnimation, slideAnimation } from "../config/motion";
 
 function Customizer() {
-  return (
-    <div></div>
-  )
+  return <div></div>;
 }
 
-export default Customizer
+export default Customizer;
