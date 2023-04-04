e93338c438d391f2b427b285fc78e092ad344950
Prarthna <prarthnaluthra@gmail.com>
2023-04-04 13:06:28 +0530

    imports homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import {motion, AnimatePresence} from 'framer-motion'
+import {useSnapshot} from 'valtio' 
+import {headContainerAnimation,headTextAnimation,headContentAnimation,slideAnimation} from '../config/motion'
 
 function Home() {
   return (
