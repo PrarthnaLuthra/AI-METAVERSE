1c9b205211adeb8432159f439e68fe5a26c05fb9
Prarthna <prarthnaluthra@gmail.com>
2023-04-04 13:57:17 +0530

    custom button

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Customizer() {
   return (
-    <div>Customizer</div>
+    <div></div>
   )
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -15,13 +15,28 @@ function Home() {
     <AnimatePresence>
       {snap.intro && (
         <motion.section className="home" {...slideAnimation("left")}>
-          <motion.header {...slideAnimation('down')}>
+          <motion.header {...slideAnimation("down")}>
             <img
               src="./tpv.png"
               alt="logo"
-              className="w-9 h-9 object-contain"
+              className="w-20 h-20 object-contain"
             />
           </motion.header>
+          <motion.div className="home-content" {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className="head-text">
+                LET'S <br className="xl:block hidden" /> STYLE IT.
+              </h1>
+            </motion.div>
+            <motion.div {...headContentAnimation} className="flex flex-col gap-5">
+              <p className="max-w-md font-normal text-gray-600 text-base">
+                Customise it, own it here at TPV with our Brand-new 3D
+                customization tool.
+                <strong> Unleash your imagination</strong> and define your own
+                style.
+              </p>
+            </motion.div>
+          </motion.div>
         </motion.section>
       )}
     </AnimatePresence>
